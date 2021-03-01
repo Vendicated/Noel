159db3b42fadad464dc68d295ea50c4a5ba963a8
Vendicated <vendicated@riseup.net>
2021-03-01 04:38:14 +0100

    Switch to import require statement

--- Noel/Util/constants.ts
+++ Noel/Util/constants.ts
@@ -1,3 +1,2 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-export const packageJson = require("../../package.json");
+export import packageJson = require("../../package.json");
 export const hastebinMirror = process.env.HASTEBIN_MIRROR || "https://hb.wrmsr.io";
