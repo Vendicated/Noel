f3f048620ef7cc1d859651b14841acbcda8e7a91
Vendicated <vendicated@riseup.net>
2021-03-01 04:42:11 +0100

    Require is for losers

--- Noel/Util/constants.ts
+++ Noel/Util/constants.ts
@@ -1,2 +1,4 @@
-export import packageJson = require("../../package.json");
+import packageJson from "../../package.json";
+export const PACKAGE_JSON = packageJson;
+
 export const hastebinMirror = process.env.HASTEBIN_MIRROR || "https://hb.wrmsr.io";
